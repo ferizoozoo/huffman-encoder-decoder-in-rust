1c72d52e70873e4dbe154b3eedbc500c40a35a05
ferizoozoo <farhadzohoorz@gmail.com>
2024-08-20 11:06:49 +0330

    feat: read file and put frequency chars into a hashmap

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+use std::{collections::HashMap, env, fs};
+
 fn main() {
-    println!("Hello, World!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 2 {
+        panic!("Filename is not provided");
+    }
+
+    let filename = args[1].clone();
+
+    let contents = fs::read_to_string(filename).expect("Could not read the file");
+    let mut freq: HashMap<char, u32> = HashMap::new();
+
+    for c in contents.chars() {
+        match freq.get(&c) {
+            Some(count) => {
+                freq.insert(c, count + 1);
+            }
+            None => {
+                freq.insert(c, 1);
+            }
+        }
+    }
 }
